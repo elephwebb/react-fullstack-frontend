785af79fb2bd8162068475e36a793f1e93fe3c19
Eleph Webb <elephwebb@gmail.com>
2019-10-27 23:47:03 -0300

    Add Materialize.css and Header styling

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,15 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-/**
- * Dummy components
- * @returns {*}
- * @constructor
- */
-const Header = () => <h2>Header</h2>
+import Header from './Header';
+
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>New Survey</h2>
 const Landing = () => <h2>Landing Page</h2>
 
 const App = () => {
  return(
-    <div>
+    <div className="container">
         <BrowserRouter>
             <div>
                 <Header />
