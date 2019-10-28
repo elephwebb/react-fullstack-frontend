ea9e2fe18d3618dc692c3db2a87ebdee3b4e94ab
Eleph Webb <elephwebb@gmail.com>
2019-10-27 21:56:40 -0300

    Set initial components router configuration

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,30 @@
+/**
+ * App Component will be responsible for the routing of our application
+ */
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+/**
+ * Dummy components
+ * @returns {*}
+ * @constructor
+ */
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>New Survey</h2>
+const Landing = () => <h2>Landing Page</h2>
 
 const App = () => {
  return(
     <div>
-        Simple code
+        <BrowserRouter>
+            <div>
+                <Header />
+                <Route exact path="/" component={Landing} />
+                <Route exact path="/surveys" component={Dashboard} />
+                <Route path="/surveys/new" component={SurveyNew} />
+            </div>
+        </BrowserRouter>
     </div>
  );
 };
