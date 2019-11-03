0b67a43b5d0f3e948f21c37cb689737c99deea3c
Eleph Webb <elephwebb@gmail.com>
2019-11-03 14:06:24 -0300

    Add conditions using React Readux Connector to decide what content show in the header area

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,30 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Header extends Component{
+
+    renderContent(){
+        switch(this.props.auth){
+            case null: // Inital state, We still have no user object defined
+                return;
+            case false:   //user is not loggedIn
+                 return (
+                    <li><a href="/auth/google">Login with Google</a></li>
+                 );
+            defautl:
+                return <li><a href="/api/logout">Logout</a></li>;
+        }
+    }
+
+
+
     render() {
         return (
             <nav>
                 <div className="nav-wrapper">
                     <a href="#" className="left brand-logo">Emaily</a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li>
-                                <a href="/auth/google">
-                                    Login with Google
-                                </a>
-                        </li>
+                        {this.renderContent()}
                     </ul>
                 </div>
             </nav>
@@ -19,4 +32,14 @@ class Header extends Component{
     }
 }
 
-export default Header;
+
+/**
+ * Map to Props the data we have defined in our Redux Store
+ * @param state
+ * @returns {{auth: *}}
+ */
+function mapStateToProps(state){
+    return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(Header);
